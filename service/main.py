ca2652e394c888d39126a56a800138a2e607b744
Lena <lena.idier@protonmail.com>
2024-08-12 19:18:47 +0200

    feat: Add retry logic when reading messages fails

--- service/main.py
+++ service/main.py
@@ -25,22 +25,27 @@ def main():
     bucket = s3.Bucket(os.environ.get("AWS_S3_BUCKET"))
 
     while 1:
-        response = sqs.receive_message(
-            QueueUrl=queue_url,
-            MaxNumberOfMessages=1,
-            WaitTimeSeconds=20,
-        )
+        print("New loop: ", datetime.datetime.now())
+        try:
+            response = sqs.receive_message(
+                QueueUrl=queue_url,
+                MaxNumberOfMessages=1,
+                WaitTimeSeconds=20,
+            )
 
-        messages = response.get("Messages")
-        if not messages:
+            messages = response.get("Messages")
+            if not messages:
+                continue
+
+            message = messages[0]
+            receipt_handle = message["ReceiptHandle"]
+            dynamodb_key, s3_key = json.loads(message["Body"]).values()
+            bucket.download_file(Key=s3_key, Filename="s3_" + s3_key)
+            laptop_item = table.get_item(Key={"uuid": dynamodb_key})["Item"]
+        except Exception as e:
+            print(f"Error: {e}")
             continue
 
-        message = messages[0]
-        receipt_handle = message["ReceiptHandle"]
-        dynamodb_key, s3_key = json.loads(message["Body"]).values()
-
-        bucket.download_file(Key=s3_key, Filename="s3_" + s3_key)
-        laptop_item = table.get_item(Key={"uuid": dynamodb_key})["Item"]
         print(laptop_item)
 
         sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
